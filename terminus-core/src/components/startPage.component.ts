8be70110fe457e59751900d83e91b462650f93f7
Eugene Pankov <john.pankov@gmail.com>
2017-04-28 17:20:21 +0200

    .

--- terminus-core/src/components/startPage.component.ts
+++ terminus-core/src/components/startPage.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core'
+import { ElectronService } from '../services/electron.service'
 import { IToolbarButton, ToolbarButtonProvider } from '../api'
 
 @Component({
@@ -8,6 +9,7 @@ import { IToolbarButton, ToolbarButtonProvider } from '../api'
 })
 export class StartPageComponent {
     constructor(
+        private electron: ElectronService,
         @Inject(ToolbarButtonProvider) private toolbarButtonProviders: ToolbarButtonProvider[],
     ) { }
 
@@ -16,5 +18,13 @@ export class StartPageComponent {
             .map(provider => provider.provide())
             .reduce((a, b) => a.concat(b))
             .sort((a: IToolbarButton, b: IToolbarButton) => (a.weight || 0) - (b.weight || 0))
+    }
+
+    openGitHub () {
+        this.electron.shell.openExternal('https://github.com/eugeny/terminus')
+    }
+
+    reportBug () {
+        this.electron.shell.openExternal('https://github.com/eugeny/terminus/issues/new')
     }
 }
