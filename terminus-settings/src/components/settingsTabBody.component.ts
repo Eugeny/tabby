fd67a3c69747aff8f09c598093328ff4e0249703
Eugene Pankov <john.pankov@gmail.com>
2017-06-03 15:07:08 +0200

    Revert "."
    This reverts commit 741f7ee8e5cb73f569e9a24c2a4e06b955510af6.

--- terminus-settings/src/components/settingsTabBody.component.ts
+++ terminus-settings/src/components/settingsTabBody.component.ts
@@ -1,10 +1,25 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, ViewContainerRef, ViewChild, ComponentFactoryResolver, ComponentRef } from '@angular/core'
 import { SettingsTabProvider } from '../api'
 
 @Component({
     selector: 'settings-tab-body',
-    template: '<ng-container *ngComponentOutlet="provider.getComponentType()"></ng-container>',
+    template: '<ng-template #placeholder></ng-template>',
 })
 export class SettingsTabBodyComponent {
     @Input() provider: SettingsTabProvider
+    @ViewChild('placeholder', {read: ViewContainerRef}) placeholder: ViewContainerRef
+    private component: ComponentRef<Component>
+
+    constructor (private componentFactoryResolver: ComponentFactoryResolver) { }
+
+    ngAfterViewInit () {
+        // run after the change detection finishes
+        setImmediate(() => {
+            this.component = this.placeholder.createComponent(
+                this.componentFactoryResolver.resolveComponentFactory(
+                    this.provider.getComponentType()
+                )
+            )
+        })
+    }
 }
