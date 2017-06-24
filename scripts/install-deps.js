bf3882b557b4a37aaf48ecae28e2a280831a0534
Eugene Pankov <john.pankov@gmail.com>
2017-06-24 15:15:45 +0200

    .

--- scripts/install-deps.js
+++ scripts/install-deps.js
@@ -6,12 +6,10 @@ const log = require('npmlog')
 
 log.info('deps', 'app')
 sh.exec('npm prune')
-sh.exec('npm install')
 sh.exec('npm update --dev')
 
 sh.cd('app')
 sh.exec('npm prune')
-sh.exec('npm install')
 sh.exec('npm update --dev')
 sh.cd('..')
 
@@ -19,7 +17,6 @@ vars.builtinPlugins.forEach(plugin => {
   log.info('deps', plugin)
   sh.cd(plugin)
   sh.exec('npm prune')
-  sh.exec('npm install')
   sh.exec('npm update --dev')
   sh.cd('..')
 })
