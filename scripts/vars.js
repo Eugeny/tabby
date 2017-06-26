b4e503b26c7f95bce8267aeeae1918ee3f8c427a
Eugene Pankov <john.pankov@gmail.com>
2017-06-26 21:54:36 +0200

    .

--- scripts/vars.js
+++ scripts/vars.js
@@ -1,8 +1,12 @@
 const path = require('path')
 const fs = require('fs')
+const childProcess = require('child_process')
 
 const appInfo = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../app/package.json')))
 const pkgInfo = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json')))
+
+exports.version = childProcess.execSync('git describe --tags', {encoding:'utf-8'})
+exports.version = exports.version.substring(1, exports.version.length - 1)
 
 exports.builtinPlugins = [
   'terminus-core',
