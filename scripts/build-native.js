100a8cacdd7a12f198b11694f5db389d65dc9761
Eugene Pankov <john.pankov@gmail.com>
2018-08-09 11:53:22 -0700

    bumped build tools

--- scripts/build-native.js
+++ scripts/build-native.js
@@ -3,18 +3,25 @@ const rebuild = require('electron-rebuild').default
 const path = require('path')
 const vars = require('./vars')
 
-rebuild({
+lifecycles = []
+lifecycles.push(rebuild({
   buildPath: path.resolve(__dirname, '../app'),
   electronVersion: vars.electronVersion,
   force: true,
-})
-rebuild({
+}).lifecycle)
+lifecycles.push(rebuild({
   buildPath: path.resolve(__dirname, '../terminus-ssh'),
   electronVersion: vars.electronVersion,
   force: true,
-})
-rebuild({
+}).lifecycle)
+lifecycles.push(rebuild({
   buildPath: path.resolve(__dirname, '../terminus-terminal'),
   electronVersion: vars.electronVersion,
   force: true,
-})
+}).lifecycle)
+
+for (let lc of lifecycles) {
+  lc.on('module-found', name => {
+    console.info('Rebuilding', name)
+  })
+}
