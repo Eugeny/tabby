a0e3b0b03312ffcb72ff6c461bbd608e537ae2a5
Eugene Pankov <john.pankov@gmail.com>
2018-10-13 14:14:39 +0200

    tell the user how to mute the WSL bell (fixes #413)

--- terminus-terminal/src/components/terminalSettingsTab.component.ts
+++ terminus-terminal/src/components/terminalSettingsTab.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
-import { ConfigService } from 'terminus-core'
+import { ConfigService, ElectronService } from 'terminus-core'
+import { TerminalService } from '../services/terminal.service'
 
 @Component({
     template: require('./terminalSettingsTab.component.pug'),
@@ -7,5 +8,16 @@ import { ConfigService } from 'terminus-core'
 export class TerminalSettingsTabComponent {
     constructor (
         public config: ConfigService,
+        private electron: ElectronService,
+        private terminal: TerminalService,
     ) { }
+
+    openWSLVolumeMixer () {
+        this.electron.shell.openItem('sndvol.exe')
+        this.terminal.openTab({
+            id: '',
+            command: 'wsl.exe',
+            args: ['tput', 'bel'],
+        }, null, true)
+    }
 }
