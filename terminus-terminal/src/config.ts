2fd28f2bf36331eec718521f3f0ff89331a5ffd0
Eugene Pankov <john.pankov@gmail.com>
2018-10-23 11:56:55 +0200

    avoid double paste on macOS

--- terminus-terminal/src/config.ts
+++ terminus-terminal/src/config.ts
@@ -65,7 +65,6 @@ export class TerminalConfigProvider extends ConfigProvider {
                     '⌘-C',
                 ],
                 'paste': [
-                    '⌘-V',
                 ],
                 'clear': [
                     '⌘-K',
