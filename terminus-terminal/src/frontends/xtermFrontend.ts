462232a2fb8145c9d0b1019e738a9c7be12cd960
Eugene Pankov <john.pankov@gmail.com>
2018-11-28 16:45:58 +0100

    xterm scrollbar styles

--- terminus-terminal/src/frontends/xtermFrontend.ts
+++ terminus-terminal/src/frontends/xtermFrontend.ts
@@ -3,6 +3,7 @@ import { Terminal, ITheme } from 'xterm'
 import * as fit from 'xterm/lib/addons/fit/fit'
 import * as ligatures from 'xterm-addon-ligatures-tmp'
 import 'xterm/dist/xterm.css'
+import './xterm.css'
 import deepEqual = require('deep-equal')
 
 Terminal.applyAddon(fit)
