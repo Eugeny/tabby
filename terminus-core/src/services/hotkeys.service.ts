798dda52363df17922fee0d82cebd57ffe93ffc4
Austin Warren <awarren@morwire.com>
2018-12-14 14:50:16 -0800

    Added rename-tab hotkey

--- terminus-core/src/services/hotkeys.service.ts
+++ terminus-core/src/services/hotkeys.service.ts
@@ -216,6 +216,10 @@ export class AppHotkeyProvider extends HotkeyProvider {
             name: 'Toggle fullscreen mode',
         },
         {
+            id: 'rename-tab',
+            name: 'Rename Tab',
+        },
+        {
             id: 'close-tab',
             name: 'Close tab',
         },
