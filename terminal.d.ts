c632f43e9484374e87832b4adedc65d9f3a8407b
Eugene Pankov <john.pankov@gmail.com>
2019-02-24 15:03:20 +0100

    fonts

--- terminal.d.ts
+++ terminal.d.ts
@@ -2,3 +2,4 @@ import 'core-js/es7/reflect';
 import '@fortawesome/fontawesome-free/css/solid.css';
 import '@fortawesome/fontawesome-free/css/brands.css';
 import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import 'source-code-pro/source-code-pro.css';
--- terminal.ts
+++ terminal.ts
@@ -3,6 +3,7 @@ import 'core-js/es7/reflect'
 import '@fortawesome/fontawesome-free/css/solid.css'
 import '@fortawesome/fontawesome-free/css/brands.css'
 import '@fortawesome/fontawesome-free/css/fontawesome.css'
+import 'source-code-pro/source-code-pro.css'
 
 async function start () {
     class NodePTY {
