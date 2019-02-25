f35fc846ec97a6e3eaec1e986aa9e93252cf84dc
Eugene Pankov <john.pankov@gmail.com>
2019-02-25 10:25:13 +0100

    fonts

--- terminal.d.ts
+++ terminal.d.ts
@@ -3,3 +3,4 @@ import '@fortawesome/fontawesome-free/css/solid.css';
 import '@fortawesome/fontawesome-free/css/brands.css';
 import '@fortawesome/fontawesome-free/css/fontawesome.css';
 import 'source-code-pro/source-code-pro.css';
+import 'source-sans-pro/source-sans-pro.css';
--- terminal.ts
+++ terminal.ts
@@ -4,6 +4,7 @@ import '@fortawesome/fontawesome-free/css/solid.css'
 import '@fortawesome/fontawesome-free/css/brands.css'
 import '@fortawesome/fontawesome-free/css/fontawesome.css'
 import 'source-code-pro/source-code-pro.css'
+import 'source-sans-pro/source-sans-pro.css'
 
 async function start () {
     class NodePTY {
