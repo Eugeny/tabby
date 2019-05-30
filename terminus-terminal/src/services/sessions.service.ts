15e6c4269261309993cf69eb8e85fc76666d639b
Eugene Pankov <john.pankov@gmail.com>
2019-05-30 11:37:55 +0200

    build fix

--- terminus-terminal/src/services/sessions.service.ts
+++ terminus-terminal/src/services/sessions.service.ts
@@ -128,7 +128,7 @@ export class Session extends BaseSession {
             cwd,
             env: env,
             // `1` instead of `true` forces ConPTY even if unstable
-            experimentalUseConpty: (isWindowsBuild(WIN_BUILD_CONPTY_SUPPORTED) && this.config.store.terminal.useConPTY) ? 1 : false,
+            experimentalUseConpty: ((isWindowsBuild(WIN_BUILD_CONPTY_SUPPORTED) && this.config.store.terminal.useConPTY) ? 1 : false) as any,
         })
 
         this.guessedCWD = cwd
