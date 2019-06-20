e18105632b9c148b96630e35e2b081d18e2e1823
Eugene Pankov <john.pankov@gmail.com>
2019-06-20 12:32:57 +0200

    set proper version number on nightly builds (fixes #1071)

--- scripts/vars.js
+++ scripts/vars.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const fs = require('fs')
+const semver = require('semver')
 const childProcess = require('child_process')
 
 const appInfo = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../app/package.json')))
@@ -8,6 +9,10 @@ const electronInfo = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../node
 exports.version = childProcess.execSync('git describe --tags', {encoding:'utf-8'})
 exports.version = exports.version.substring(1).trim()
 exports.version = exports.version.replace('-', '-c')
+
+if (exports.version.includes('-c')) {
+  exports.version = semver.inc(exports.version, 'prepatch').replace('-0', '-nightly.0')
+}
 
 exports.builtinPlugins = [
   'terminus-core',
