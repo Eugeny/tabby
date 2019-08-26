bd713f8e07355c056ee4accf669de2efbc7c50c6
Eugene Pankov <john.pankov@gmail.com>
2019-08-26 23:52:20 +0200

    ci

--- scripts/build-windows.js
+++ scripts/build-windows.js
@@ -3,14 +3,21 @@ const builder = require('electron-builder').build
 const vars = require('./vars')
 
 const isTag = (process.env.GITHUB_REF || '').startsWith('refs/tags/')
+const isCI = !!process.env.GITHUB_REF
 
 builder({
     dir: true,
     win: ['nsis', 'portable'],
     config: {
+        publish: isTag ? [
+            { provider: 'bintray', component: 'main' },
+            { provider: 'github' },
+        ] : [
+            { provider: 'bintray', component: 'nightly' },
+        ],
         extraMetadata: {
             version: vars.version,
         },
     },
-    publish: isTag ? 'always' : 'onTag',
+    publish: isCI ? 'always' : 'onTag',
 }).catch(() => process.exit(1))
