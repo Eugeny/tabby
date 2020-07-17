dbda8dad34b9fce677ff5bbad0c28d9c8bf7f785
Eugene Pankov <john.pankov@gmail.com>
2020-07-17 20:45:18 +0200

    lint

--- terminus-ssh/src/api.ts
+++ terminus-ssh/src/api.ts
@@ -161,7 +161,7 @@ export class SSHSession extends BaseSession {
             }
         })
 
-        this.shell.on('end', e => {
+        this.shell.on('end', () => {
             this.logger.info('Shell session ended')
             if (this.open) {
                 this.destroy()
