a24f52c58a9f7e2170a4bb214914948ba236cf1f
Eugene Pankov <john.pankov@gmail.com>
2020-12-24 18:18:07 +0100

    more build fixes

--- scripts/build-macos.js
+++ scripts/build-macos.js
@@ -6,10 +6,12 @@ const signHook = require('../build/mac/afterSignHook')
 
 const isTag = (process.env.GITHUB_REF || '').startsWith('refs/tags/')
 
+process.env.ARCH = process.env.ARCH || process.arch
+
 builder({
     dir: true,
     mac: ['pkg', 'zip'],
-    arm64: (process.env.ARCH ?? process.arch) === 'arm64',
+    arm64: process.env.ARCH === 'arm64',
     config: {
         extraMetadata: {
             version: vars.version,
