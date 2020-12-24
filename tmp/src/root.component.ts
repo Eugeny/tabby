9fddfa6fc24575977d8360c4b852eb2534853b0b
Eugene Pankov <john.pankov@gmail.com>
2020-12-24 16:10:31 +0100

    removed temp files

--- tmp/src/root.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-    template: '<app-root></app-root>',
-})
-export class RootComponent { } // eslint-disable-line @typescript-eslint/no-extraneous-class
