cbcd0fea616d49a18bb200a24683704b19683a5a
Eugene Pankov <john.pankov@gmail.com>
2021-05-16 20:17:40 +0200

    lint

--- terminus-serial/src/components/editConnectionModal.component.ts
+++ terminus-serial/src/components/editConnectionModal.component.ts
@@ -53,7 +53,7 @@ export class EditConnectionModalComponent {
     baudratesAutocomplete = text$ => text$.pipe(
         debounceTime(200),
         distinctUntilChanged(),
-        map(q => BAUD_RATES.filter(x => !q || x.toString().startsWith(q.toString())))
+        map((q: string) => BAUD_RATES.filter(x => !q || x.toString().startsWith(q)))
     )
 
     portsFormatter = port => {
