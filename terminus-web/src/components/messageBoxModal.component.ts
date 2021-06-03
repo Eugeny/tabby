2b4c25104ff988e7894ba5a7f8ead2b4eb9e4a06
Eugene Pankov <john.pankov@gmail.com>
2021-06-03 22:48:48 +0200

    lint

--- terminus-web/src/components/messageBoxModal.component.ts
+++ terminus-web/src/components/messageBoxModal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ElementRef, } from '@angular/core'
+import { Component, Input, ElementRef } from '@angular/core'
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap'
 import { BaseComponent, HotkeysService, MessageBoxOptions } from 'terminus-core'
 
