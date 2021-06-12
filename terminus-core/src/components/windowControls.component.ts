a2ed674b108d78fc3c914607fe2577892b969f1e
Eugene Pankov <john.pankov@gmail.com>
2021-06-12 23:21:17 +0200

    fixed window controls behaviour - fixes #3984

--- terminus-core/src/components/windowControls.component.ts
+++ terminus-core/src/components/windowControls.component.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Component } from '@angular/core'
-import { HostAppService } from '../services/hostApp.service'
+import { HostWindowService } from '../api/hostWindow'
 import { AppService } from '../services/app.service'
 
 /** @hidden */
@@ -10,7 +10,7 @@ import { AppService } from '../services/app.service'
     styles: [require('./windowControls.component.scss')],
 })
 export class WindowControlsComponent {
-    private constructor (public hostApp: HostAppService, public app: AppService) { }
+    private constructor (public hostWindow: HostWindowService, public app: AppService) { }
 
     async closeWindow () {
         this.app.closeWindow()
