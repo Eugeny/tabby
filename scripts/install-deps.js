88015746500cb27d232c7b00a19fa2f8f584e6c1
Eugene Pankov <john.pankov@gmail.com>
2021-06-19 22:01:35 +0200

    Update install-deps.js

--- scripts/install-deps.js
+++ scripts/install-deps.js
@@ -13,6 +13,10 @@ sh.cd('app')
 sh.exec(`${npx} yarn install --force`)
 sh.cd('..')
 
+sh.cd('web')
+sh.exec(`${npx} yarn install --force`)
+sh.cd('..')
+
 vars.builtinPlugins.forEach(plugin => {
   log.info('deps', plugin)
   sh.cd(plugin)
