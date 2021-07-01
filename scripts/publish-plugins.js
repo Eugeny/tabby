db8c534d0a704705412ee8f8d852aa867486b6ad
Eugene Pankov <john.pankov@gmail.com>
2021-07-01 22:56:23 +0200

    bumped plugin versions

--- scripts/publish-plugins.js
+++ scripts/publish-plugins.js
@@ -4,8 +4,9 @@ const vars = require('./vars')
 const log = require('npmlog')
 
 vars.builtinPlugins.forEach(plugin => {
-  log.info('bump', plugin)
-  sh.cd(plugin)
-  sh.exec('npm --no-git-tag-version version ' + vars.version)
-  sh.cd('..')
+    log.info('bump', plugin)
+    sh.cd(plugin)
+    sh.exec('npm --no-git-tag-version version ' + vars.version)
+    sh.exec('npm publish')
+    sh.cd('..')
 })
