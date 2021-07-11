d1a6baf8584d800ced1ecd8384fdc624168f35f2
Eugene Pankov <john.pankov@gmail.com>
2021-07-11 23:03:55 +0200

    bump

--- scripts/publish-plugins.js
+++ scripts/publish-plugins.js
@@ -3,7 +3,7 @@ const sh = require('shelljs')
 const vars = require('./vars')
 const log = require('npmlog')
 
-vars.builtinPlugins.forEach(plugin => {
+vars.allPackages.forEach(plugin => {
     log.info('bump', plugin)
     sh.cd(plugin)
     sh.exec('npm --no-git-tag-version version ' + vars.version)
