aa62ca9a6e0ac665e833b630db518f799b9e0432
Eugene Pankov <e@ajenti.org>
2021-12-06 18:12:54 +0100

    Update ssh.ts

--- tabby-ssh/src/session/ssh.ts
+++ tabby-ssh/src/session/ssh.ts
@@ -24,7 +24,7 @@ const WINDOWS_OPENSSH_AGENT_PIPE = '\\\\.\\pipe\\openssh-ssh-agent'
 
 export interface Prompt {
     prompt: string
-    echo: boolean
+    echo?: boolean
 }
 
 interface AuthMethod {
