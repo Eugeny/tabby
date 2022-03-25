37ba06b65ce78160882216d65d163831bb5b5e63
Jai A P <78354625+Jai-JAP@users.noreply.github.com>
2022-03-25 16:25:06 +0530

    Update build-linux.js

--- scripts/build-linux.js
+++ scripts/build-linux.js
@@ -4,17 +4,17 @@ const vars = require('./vars')
 
 const isTag = (process.env.GITHUB_REF || '').startsWith('refs/tags/')
 
-process.env.ARCH = process.env.ARCH || process.arch
+process.env.ARCH = process.env.ARCH || process.arch === 'arm' ? armv7l : process.arch
 
 builder({
     dir: true,
     linux: ['deb', 'tar.gz', 'rpm', 'pacman'],
+    armv7l: process.env.ARCH === 'armv7l',
     arm64: process.env.ARCH === 'arm64',
     config: {
         extraMetadata: {
             version: vars.version,
         },
-        npmRebuild: process.env.ARCH == 'arm64',
     },
     publish: isTag ? 'always' : 'onTag',
 }).catch(() => process.exit(1))
