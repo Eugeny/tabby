ff82ed2e8b4e69ff2e9ff82a2dd2a2817752ec24
woodmeal <>
2022-04-22 09:27:24 +0200

    replaced String.charAt() by String.startsWith() method to fix linter error

--- tabby-ssh/src/session/ssh.ts
+++ tabby-ssh/src/session/ssh.ts
@@ -306,7 +306,7 @@ export class SSHSession {
                     this.authUsername = 'root'
                 }
             }
-            if (this.authUsername && this.authUsername.charAt(0) === '$') {
+            if (this.authUsername && this.authUsername.startsWith('$')) {
                 try {
                     const result = process.env[this.authUsername.slice(1)]
                     this.authUsername = result ?? this.authUsername
