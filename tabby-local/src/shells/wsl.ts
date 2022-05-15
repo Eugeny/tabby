a5fb7262063170fd11a1015a15e094d3ff06c198
Liangcheng Juves <liangchengj@outlook.com>
2022-05-15 13:56:57 +0800

    Use linux icon by default when no WSL profile icon is found

--- tabby-local/src/shells/wsl.ts
+++ tabby-local/src/shells/wsl.ts
@@ -62,7 +62,8 @@ export class WSLShellProvider extends ShellProvider {
                         TERM: 'xterm-color',
                         COLORTERM: 'truecolor',
                     },
-                    icon: wslIconMap[defaultDistKey.DistributionName.value],
+                    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+                    icon: wslIconMap[defaultDistKey.DistributionName.value] ?? wslIconMap.Linux,
                 }
                 shells.push(shell)
             }
@@ -103,7 +104,8 @@ export class WSLShellProvider extends ShellProvider {
                     TERM: 'xterm-color',
                     COLORTERM: 'truecolor',
                 },
-                icon: wslIconMap[name],
+                // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+                icon: wslIconMap[name] ?? wslIconMap.Linux,
             }
             shells.push(shell)
         }
