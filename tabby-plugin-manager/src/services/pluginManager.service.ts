805d457f684cc6c6384513e6694302acf89f3323
Thomas LACAZE <19855907+LacazeThomas@users.noreply.github.com>
2022-08-20 21:33:29 +0200

    fix isOfficial bool condition

--- tabby-plugin-manager/src/services/pluginManager.service.ts
+++ tabby-plugin-manager/src/services/pluginManager.service.ts
@@ -62,7 +62,7 @@ export class PluginManagerService {
                     version: item.package.version,
                     homepage: item.package.links.homepage,
                     author: (item.package.author || {}).name,
-                    isOfficial: item.package.publisher.name === OFFICIAL_NPM_ACCOUNT,
+                    isOfficial: item.package.publisher.username === OFFICIAL_NPM_ACCOUNT,
                 }))
             ),
             map(plugins => plugins.filter(x => x.packageName.startsWith(namePrefix))),
