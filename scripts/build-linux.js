8fc3a390a3f3a438034cc547f6fff36a9e95de0c
Jai A P <jai.jap.318@gmail.com>
2022-08-30 11:50:32 +0530

    Publish only if GH_TOKEN || GITHUB_TOKEN

--- scripts/build-linux.js
+++ scripts/build-linux.js
@@ -16,12 +16,12 @@ builder({
         extraMetadata: {
             version: vars.version,
         },
-        publish: {
-            provider: 'github',
-            channel: `latest-${process.arch}`,
-        },
-    },
-    publish: isTag ? 'always' : 'onTagOrDraft',
+        publish: process.env.GH_TOKEN || process.env.GITHUB_TOKEN ? { 
+             provider: 'github', 
+             channel: `latest-${process.env.ARCH}`, 
+         } : undefined, 
+     }, 
+      publish: ( process.env.GH_TOKEN || process.env.GITHUB_TOKEN ) ? ( isTag ? 'always' : 'onTagOrDraft' ) : 'never',
 }).catch(e => {
     console.error(e)
     process.exit(1)
