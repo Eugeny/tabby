24f8a4bd439ec69bbda0447a233cb187543516f5
Eugene Pankov <e@ajenti.org>
2023-01-08 20:09:26 +0100

    fixed #7755 - prevent release notes links from navigating away

--- tabby-core/src/directives/fastHtmlBind.directive.ts
+++ tabby-core/src/directives/fastHtmlBind.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, Input, ElementRef, OnChanges } from '@angular/core'
+import { PlatformService } from '../api/platform'
 
 /** @hidden */
 @Directive({
@@ -6,9 +7,19 @@ import { Directive, Input, ElementRef, OnChanges } from '@angular/core'
 })
 export class FastHtmlBindDirective implements OnChanges {
     @Input() fastHtmlBind: string
-    constructor (private el: ElementRef) { }
+
+    constructor (
+        private el: ElementRef,
+        private platform: PlatformService,
+    ) { }
 
     ngOnChanges (): void {
         this.el.nativeElement.innerHTML = this.fastHtmlBind || ''
+        for (const link of this.el.nativeElement.querySelectorAll('a')) {
+            link.addEventListener('click', event => {
+                event.preventDefault()
+                this.platform.openExternal(link.href)
+            })
+        }
     }
 }
