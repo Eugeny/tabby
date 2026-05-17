a9da4c14b0515eaebba9c7cac875c06772770a95
Aftab3008 <shaikaftab005@gmail.com>
2026-05-17 22:56:38 +0530

    fix: Warn when closing active conn (ssh)

--- tabby-ssh/src/components/sshTab.component.ts
+++ tabby-ssh/src/components/sshTab.component.ts
@@ -198,7 +198,7 @@ export class SSHTabComponent extends ConnectableTerminalTabComponent<SSHProfile>
         if (!this.session?.open) {
             return true
         }
-        if (!this.profile.options.warnOnClose) {
+        if (!(this.profile.options.warnOnClose ?? this.config.store.ssh.warnOnClose)) {
             return true
         }
         return (await this.platform.showMessageBox(
