ec630984606c37e6c6994e33b342643ca3358234
orbisai0security <mediratta01.pally@gmail.com>
2026-05-29 17:39:39 +0000

    fix: V-003 security vulnerability
    Automated security fix generated by OrbisAI Security

--- tabby-plugin-manager/src/services/pluginManager.service.ts
+++ tabby-plugin-manager/src/services/pluginManager.service.ts
@@ -51,7 +51,7 @@ export class PluginManagerService {
 
     _listAvailableInternal (namePrefix: string, keyword: string, query?: string): Observable<PluginInfo[]> {
         return from(
-            axios.get(`https://registry.npmjs.com/-/v1/search?text=keywords%3A${keyword}%20${query}&size=250`),
+            axios.get(`https://registry.npmjs.com/-/v1/search?text=keywords%3A${encodeURIComponent(keyword)}%20${encodeURIComponent(query ?? '')}&size=250`),
         ).pipe(
             map(response => response.data.objects
                 .filter(item => !item.keywords?.includes('tabby-dummy-transition-plugin'))
