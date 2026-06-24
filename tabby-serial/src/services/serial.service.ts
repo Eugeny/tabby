eaefc794cc81877fced95894ad2af511c5246485
bluekani <24561326+bluekani@users.noreply.github.com>
2026-06-24 21:28:20 +0900

    fix(serial): fix serial quickConnect not working

--- tabby-serial/src/services/serial.service.ts
+++ tabby-serial/src/services/serial.service.ts
@@ -1,15 +1,13 @@
-import { Injectable, Injector } from '@angular/core'
+import { Injectable } from '@angular/core'
 import WSABinding from 'serialport-binding-webserialapi'
 import AbstractBinding from '@serialport/binding-abstract'
 import { autoDetect } from '@serialport/bindings-cpp'
-import { HostAppService, PartialProfile, Platform, ProfilesService } from 'tabby-core'
-import { SerialPortInfo, SerialProfile } from '../api'
-import { SerialTabComponent } from '../components/serialTab.component'
+import { HostAppService, Platform } from 'tabby-core'
+import { SerialPortInfo } from '../api'
 
 @Injectable({ providedIn: 'root' })
 export class SerialService {
     private constructor (
-        private injector: Injector,
         private hostApp: HostAppService,
     ) { }
 
@@ -27,24 +25,5 @@ export class SerialService {
             console.error('Failed to list serial ports', err)
             return []
         }
-    }
-
-    quickConnect (query: string): Promise<SerialTabComponent|null> {
-        let path = query
-        let baudrate = 115200
-        if (query.includes('@')) {
-            baudrate = parseInt(path.split('@')[1])
-            path = path.split('@')[0]
-        }
-        const profile: PartialProfile<SerialProfile> = {
-            name: query,
-            type: 'serial',
-            options: {
-                port: path,
-                baudrate: baudrate,
-            },
-        }
-        window.localStorage.lastSerialConnection = JSON.stringify(profile)
-        return this.injector.get(ProfilesService).openNewTabForProfile(profile) as Promise<SerialTabComponent|null>
     }
 }
