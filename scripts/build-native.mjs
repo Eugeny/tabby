508dbc298004c811fdbd457d93bbd74ebbff6ac0
Liao Xin <2935437378@qq.com>
2026-08-23 14:45:40 +0800

    fix(local): 修复 Windows TUI 鼠标滚动

--- scripts/build-native.mjs
+++ scripts/build-native.mjs
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { rebuild } from '@electron/rebuild'
+import * as fs from 'fs/promises'
 import * as path from 'path'
 import * as vars from './vars.mjs'
 
@@ -14,6 +15,7 @@ if (process.platform === 'win32' || process.platform === 'linux') {
 }
 
 let lifecycles = []
+let builds = []
 for (let dir of ['app', 'tabby-core', 'tabby-local', 'tabby-ssh', 'tabby-terminal']) {
     const build = rebuild({
         buildPath: path.resolve(__dirname, '../' + dir),
@@ -25,6 +27,7 @@ for (let dir of ['app', 'tabby-core', 'tabby-local', 'tabby-ssh', 'tabby-termina
         console.error(e)
         process.exit(1)
     })
+    builds.push(build)
     lifecycles.push([build.lifecycle, dir])
 }
 
@@ -34,4 +37,19 @@ for (let [lc, dir] of lifecycles) {
     lc.on('module-found', name => {
         console.info('Rebuilding', dir + '/' + name)
     })
+}
+
+await Promise.all(builds)
+
+if (process.platform === 'win32') {
+    const nodePtyRoot = path.resolve(__dirname, '../app/node_modules/node-pty')
+    const conptyRoot = path.join(nodePtyRoot, 'third_party/conpty')
+    const [conptyVersion] = await fs.readdir(conptyRoot)
+    const source = path.join(conptyRoot, conptyVersion, `win10-${process.env.ARCH}`)
+    const destination = path.join(nodePtyRoot, 'build/Release/conpty')
+
+    await fs.mkdir(destination, { recursive: true })
+    for (const file of ['conpty.dll', 'OpenConsole.exe']) {
+        await fs.copyFile(path.join(source, file), path.join(destination, file))
+    }
 }
