30f7ec98b958ffaf166d7e8b89aea214d2822d3c
Eugene <inbox@null.page>
2026-09-11 21:42:42 +0200

    Update baseTerminalTab.component.ts

--- tabby-terminal/src/api/baseTerminalTab.component.ts
+++ tabby-terminal/src/api/baseTerminalTab.component.ts
@@ -263,7 +263,7 @@ export class BaseTerminalTabComponent<P extends BaseTerminalProfile> extends Bas
                         const shellArgs: string[] = tabProfile.options?.args ?? []
                         if (this.hostApp.platform === Platform.Windows && (shellType === 'powershell' || shellArgs.some(arg => arg.includes('clink')))) {
                             // Windows PowerShell and cmd(Clink): send Ctrl+L to PTY (natively clears)
-                            tab.session?.write(Buffer.from('\x0c'))
+                            tab.sendInput('\x0c')
                         } else {
                             // Windows cmd(stock) and macOS/Linux: clear xterm buffer only
                             tab.frontend?.clear()
